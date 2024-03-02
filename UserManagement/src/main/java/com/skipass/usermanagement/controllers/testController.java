8d45cae5d31f0347312295ec9f2b2b0759c3412a
Aleks <akaralekereks@gmail.com>
2024-03-02 12:03:36 +0100

    working skeleton example of service

--- UserManagement/src/main/java/com/skipass/usermanagement/controllers/testController.java
+++ UserManagement/src/main/java/com/skipass/usermanagement/controllers/testController.java
@@ -1,2 +1,16 @@
-package com.skipass.usermanagement.controllers;public class testConroller {
+package com.skipass.usermanagement.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000/", allowCredentials = "true")
+
+public class testController {
+    @GetMapping("/")
+    public String test() {
+        return "Hi, im a test controller";
+    }
+
 }
