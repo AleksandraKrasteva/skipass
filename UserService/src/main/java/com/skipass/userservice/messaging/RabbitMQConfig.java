4e075865a1d9f7d890686c592252a690be6fafca
Aleks <akaralekereks@gmail.com>
2024-04-03 19:02:47 +0200

    update RabbitMQ config for userService

--- UserService/src/main/java/com/skipass/userservice/messaging/RabbitMQConfig.java
+++ UserService/src/main/java/com/skipass/userservice/messaging/RabbitMQConfig.java
@@ -4,11 +4,12 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
+@EnableRabbit
 public class RabbitMQConfig {
 
     static final String fanoutExchangeName = "delete-profile";
--- UserService/src/test/java/com/skipass/userservice/BaseClass.java
+++
@@ -1,4 +0,0 @@
-package com.skipass.userservice;
-
-public class BasicClass {
-}
