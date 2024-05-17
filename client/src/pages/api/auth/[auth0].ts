83fe49edbbafd74aeae13a1eca81b95d08341bf0
Aleks <akaralekereks@gmail.com>
2024-05-17 21:55:06 +0200

    auth workaround

--- client/src/pages/api/auth/[auth0].ts
+++ client/src/pages/api/auth/[auth0].ts
@@ -1,3 +1,25 @@
+// import { handleAuth } from '@auth0/nextjs-auth0';
+
+// export const GET = handleAuth();
+
+// // pages/api/auth/[...auth0].ts
+// export const runtime = 'edge';
+
+import { NextApiRequest, NextApiResponse } from 'next';
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+	try {
+		switch (req.method) {
+		case 'GET':
+			await handleAuth()(req, res);
+			break;
+			// Handle other methods and cases if necessary
+		default:
+			res.setHeader('Allow', ['GET']);
+			res.status(405).end(`Method ${req.method} Not Allowed`);
+		}
+	} catch (error: any) {
+		res.status(500).json({ error: 'Authentication Error', details: error.message });
+	}
+}
