36c751dcf8d945827d0543dc63022c997ca0b6fd
Aleks <akaralekereks@gmail.com>
2024-06-06 12:08:42 +0200

    update kubernetes files

--- docker_kubernetes/initialize-redeploy.ps1
+++ docker_kubernetes/initialize-redeploy.ps1
@@ -1,4 +1,1 @@
-kubectl apply -f ./app/databases/global-config.yaml;
-kubectl apply -f ./app/databases/local-storage.yaml;
-kubectl apply -f ./app/databases/post-volume.yaml;
-kubectl apply -f ./app/databases/gateway.yaml;
+kubectl apply -f ./app/config/; 
