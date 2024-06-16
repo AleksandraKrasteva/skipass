2fcc5b38e236c29550c9184c486ce7f81b92e508
Aleks <akaralekereks@gmail.com>
2024-06-16 22:53:43 +0200

    fix  journey service

--- JourneyService/src/main/java/com/skipass/journeyservice/messaging/RabbitMQReceiver.java
+++ JourneyService/src/main/java/com/skipass/journeyservice/messaging/RabbitMQReceiver.java
@@ -1,12 +1,12 @@
-package com.skipass.journeyservice.messaging;
-
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public class RabbitMQReceiver {
-    @RabbitListener(queues = "delete-profile-journey")
-    public void receiveMessage(String message) {
-        System.out.println("Received journeys for user:: " + message);
-    }
-}
+//package com.skipass.journeyservice.messaging;
+//
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class RabbitMQReceiver {
+//    @RabbitListener(queues = "delete-profile-journey")
+//    public void receiveMessage(String message) {
+//        System.out.println("Received journeys for user:: " + message);
+//    }
+//}
